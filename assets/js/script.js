0a5f2b6e19952e985770e74bca524fe990f2a83d
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-19 15:38:19 -0500

    iterate through 5 day forecast and add uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,42 @@ var windEl = document.querySelector(".wind");
 var humidityEl = document.querySelector(".humidity");
 var uvEl = document.querySelector(".uv");
 var iconEl = document.querySelector(".icon");
+var forecastEl = document.querySelector(".weather-days");
 
 var currentDisplay = function (data){
     console.log(data);
     cityEl.textContent = data.name + moment().format(" (MM/DD/YYYY)");
-    tempEl.textContent = data.main.temp;
-    windEl.textContent = data.wind.speed;
-    humidityEl.textContent = data.main.humidity;
+    tempEl.innerHTML = data.main.temp + " &deg;F";
+    windEl.textContent = data.wind.speed + " MPH";
+    humidityEl.textContent = data.main.humidity + "%";
+    iconEl.setAttribute("src","http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
 }
 
-
+var forecastDisplay = function(data){
+    console.log(data);
+    uvEl.textContent = data.current.uvi;
+    for (var i = 1; i<=5; i++){
+    var dayblockEl = document.createElement("section");
+    dayblockEl.classList = "col weather-day";
+    var dateEl = document.createElement("p");
+    dateEl.textContent = moment().add(i,"days").format("MM/DD/YYYY");
+    var dailyIconEl = document.createElement("img");
+    dailyIconEl.className = "icon";
+    dailyIconEl.setAttribute("src","http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+"@2x.png");
+    var dailyTempEl = document.createElement("p");
+    dailyTempEl.innerHTML = "Temp: " + parseInt(data.daily[i].temp.max) + "/" + parseInt(data.daily[i].temp.min) + " &deg;F";
+    var dailyWindEl = document.createElement("p");
+    dailyWindEl.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+    var dailyHumidityEl = document.createElement("p");
+    dailyHumidityEl.textContent = "Humidity: " + data.daily[i].humidity +"%";
+    dayblockEl.appendChild(dateEl);
+    dayblockEl.appendChild(dailyIconEl);
+    dayblockEl.appendChild(dailyTempEl);
+    dayblockEl.appendChild(dailyWindEl);
+    dayblockEl.appendChild(dailyHumidityEl);
+    forecastEl.appendChild(dayblockEl);
+}
+}
 
 var getWeather = function (){
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=1a4b53ff8d8ec9391998ec0c0478d866";
@@ -34,7 +60,7 @@ var getForecast = function(){
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data);
+                forecastDisplay(data);
             });
         } else{
             alert("Error: " + response.statusText);
